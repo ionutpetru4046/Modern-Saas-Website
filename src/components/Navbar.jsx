2cbd341b33940f3affe987378d002af817ac8a16
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-21 12:10:57 +0100

    navbar improved, remix icon dependencies installed, and added to json files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import logo from "../assets/logo.png"
+import { RiCloseFill, RiMenu3Line } from "@remixicon/react"
 
 const NAvbar = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -30,7 +31,27 @@ const NAvbar = () => {
             </div>
 
             {/* Right: Button (hidden on mobile) */}
-            <div className=""></div>
+            <div className="hidden md:flex space-x-4 items-center">
+                <a href="#" className="hover:text-neutral-200">
+                    Login
+                </a>
+                <a href="#" className="border border-neutral-700 text-white 
+                py-2 px-4 rounded-lg hover:bg-neutral-700 transition">
+                    Get a Demo
+                </a>
+                <a href="#" className="bg-blue-600 text-white 
+                py-2 px-4 rounded-lg hover:bg-blue-500 transition">
+                    Start Free Trial
+                </a>
+            </div>
+
+            {/* Hamburger Icon for Mobile */}
+            <div className="md:hidden">
+                <button onClick={toggleMenu} className="text-white 
+                focus:outline-none" aria-label={isOpen ? "Close Menu" : "Open Menu"}>
+                    {isOpen ? <RiCloseFill /> : <RiMenu3Line />}
+                </button>
+            </div>
         </div>
     </nav>
   )
