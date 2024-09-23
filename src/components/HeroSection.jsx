4e366a4229159c5d61900b3f96fbc58fbc1fb49f
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-23 11:41:15 +0100

    framer-motion animation added to hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,31 @@
+/* eslint-disable no-unused-vars */
+
 import { BRAND_LOGOS, HERO_CONTENT } from "../constants"
 import heroImage from "../assets/hero.jpg"
+import { motion } from "framer-motion"
+
+const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { straggerChildren: 0.2 }},
+}
+
+const fadeInUp = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 , transition: { duration: 0.6 }}
+}
+
+const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: {duration: 0.6 }}
+}
 
 const HeroSection = () => {
   return (
-    <section className="pt-28 lg:pt-36">
+    <motion.section 
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible"
+    className="pt-28 lg:pt-36">
         <div className="max-w-7xl mx-auto px-4 flex flex-col items-center">
             <div className="mb-8 border-neutral-800 px-3 py-2 rounded-full text-xs">
                 {HERO_CONTENT.badgeText}
@@ -47,7 +69,7 @@ const HeroSection = () => {
                 className="w-full h-auto rounded-3xl border border-neutral-800"/>
             </div>
         </div>
-    </section>
+    </motion.section>
   )
 }
 
