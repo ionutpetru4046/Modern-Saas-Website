0ce7d8dcdfc31742d55c786018473e0c74f52aeb
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-23 13:12:29 +0100

    new function added to the section

--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -1,4 +1,27 @@
+import { motion } from "framer-motion"
+
 const KeyFeatures = () => {
+    const containerVariants = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                straggerChildren: 0.2,
+                when: "beforeChildren",
+            }
+        }
+    }
+
+    const featureVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.5,
+            }
+        }
+    }
   return (
     <section className="">
         
