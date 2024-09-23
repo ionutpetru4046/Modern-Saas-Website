b8e5a80f4bf95a8b4ec6b5668ea25212dc71d9a4
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-23 13:36:23 +0100

    KeyFEatures section finished

--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion"
+import { KEY_FEATURES_CONTENT } from "../constants"
 
 const KeyFeatures = () => {
     const containerVariants = {
@@ -23,8 +24,41 @@ const KeyFeatures = () => {
         }
     }
   return (
-    <section className="">
-        
+    <section>
+        <div className="max-w-7xl mx-auto px-4 mt-20">
+            <div className="text-center mb-12 border-t border-neutral-800">
+                <h2 className="text-3xl lg:text-5xl mt-20 tracking-tighter 
+                bg-gradient-to-t from-neutral-50 via-neutral-300 to-neutral-600 
+                bg-clip-text text-transparent">
+                    {KEY_FEATURES_CONTENT.sectionTitle}
+                </h2>
+                <p className="mt-4">
+                    {KEY_FEATURES_CONTENT.sectionDescription}
+                </p>
+            </div>
+
+            <motion.div 
+            variants={containerVariants}
+            initial="hidden"
+            whileInView="visible"
+            className="flex flex-wrap justify-between">
+                {KEY_FEATURES_CONTENT.features.map((feature) => (
+                    <motion.div key={feature.id} className="flex flex-col items-center 
+                    text-center w-full md:w-1/2 lg:w-1/3 p-6" 
+                    variants={featureVariants}>
+                        <div className="flex justify-center items-center mb-4">
+                            {feature.icon}
+                        </div>
+                        <h3 className="text-xl">
+                            {feature.title}
+                        </h3>
+                        <p className="mt-2 text-neutral-400"> 
+                            {feature.description}
+                        </p>
+                    </motion.div>
+                ))}
+            </motion.div>
+        </div>
     </section>
   )
 }
