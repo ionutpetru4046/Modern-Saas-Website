f48275874aa9bff94460620cb3f2be79afd93809
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-23 11:50:14 +0100

    motion-framer added to hero section completed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-
 import { BRAND_LOGOS, HERO_CONTENT } from "../constants"
 import heroImage from "../assets/hero.jpg"
 import { motion } from "framer-motion"
@@ -27,10 +25,12 @@ const HeroSection = () => {
     animate="visible"
     className="pt-28 lg:pt-36">
         <div className="max-w-7xl mx-auto px-4 flex flex-col items-center">
-            <div className="mb-8 border-neutral-800 px-3 py-2 rounded-full text-xs">
+            <motion.div 
+            variants={fadeInUp}
+            className="mb-8 border-neutral-800 px-3 py-2 rounded-full text-xs">
                 {HERO_CONTENT.badgeText}
-            </div>
-            <h1 className="text-5xl lg:text-8xl my-4 font-semibold tracking-tighter 
+            </motion.div>
+            <motion.h1 className="text-5xl lg:text-8xl my-4 font-semibold tracking-tighter 
             bg-gradient-to-b from-neutral-50 via-neutral-300 to-neutral-600 bg-clip-text text-transparent">
                 {HERO_CONTENT.mainHeading.split("\n").map((text, index) => (
                     <span key={index}>
@@ -38,12 +38,16 @@ const HeroSection = () => {
                         <br />
                     </span>
                 ))}
-            </h1>
+            </motion.h1>
 
-            <p className="mt-6 text-neutral-400 max-w-xl">
+            <motion.p 
+            variants={fadeInUp}
+            className="mt-6 text-neutral-400 max-w-xl">
                 {HERO_CONTENT.subHeading}
-            </p>
-            <div className="mt-6 space-x-4">
+            </motion.p>
+            <motion.div 
+            variants={fadeInUp}
+            className="mt-6 space-x-4">
                 <a href="#" className="inline-block bg-blue-600 hover:bg-blue-500 
                 text-white py-3 px-6 rounded-lg font-medium">
                     {HERO_CONTENT.callToAction.primary}
@@ -52,22 +56,29 @@ const HeroSection = () => {
                 text-white py-3 px-6 rounded-lg font-medium">
                     {HERO_CONTENT.callToAction.secondary}
                 </a>
-            </div>
-            <div className="py-10 ">
+            </motion.div>
+
+            <motion.div 
+            variants={fadeIn}
+            className="py-10 ">
                 <p className="text-gray-400 text-center mb-8">
                     {HERO_CONTENT.trustedByText}
                 </p>
-                <div className="flex flex-wrap justify-center gap-8">
+                <motion.div 
+                variants={fadeInUp}
+                className="flex flex-wrap justify-center gap-8">
                     {BRAND_LOGOS.map((logo, index) => (
                         <img key={index} src={logo.src} alt={logo.alt} className="h-8" />
                     ))}
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
 
-            <div className="mt-12 ">
+            <motion.div 
+            variants={fadeIn}
+            className="mt-12 ">
                 <img src={heroImage} alt="Steamer Saas Interface" 
                 className="w-full h-auto rounded-3xl border border-neutral-800"/>
-            </div>
+            </motion.div>
         </div>
     </motion.section>
   )
