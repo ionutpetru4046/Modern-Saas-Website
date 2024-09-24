4d10b36a7ccdc5414f0a72c34f663319366ed983
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-24 12:18:18 +0100

    footer section finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,39 @@
+import { FOOTER_CONTENT } from "../constants"
+
 const Footer = () => {
   return (
-    <div>
-        
-    </div>
+    <footer className="mt-20 text-neutral-400">
+        <div className="max-w-7xl px-4 border-t border-neutral-800">
+            <div className="grid grid-cols-1 md:grid-cols-4 mt-20">
+                {FOOTER_CONTENT.sections.map((section, index) => (
+                    <div key={index}>
+                        <h3 className="text-white font-medium mb-4">
+                            {section.title}
+                        </h3>
+                        <ul className="space-y-2">
+                            {section.links.map((link, index) => (
+                                <li key={index}>
+                                    <a href={link.url}>{link.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+
+            <div className="my-12 border-t border-neutral-800 pt-8 text-center 
+            text-sm text-neutral-50">
+                <div className="flex justify-between">
+                    <div className="text-xs">
+                        <p>{FOOTER_CONTENT.platformsText}</p>
+                    </div>
+                    <div className="text-xs">
+                        <p>{FOOTER_CONTENT.copyrightText}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
   )
 }
 
