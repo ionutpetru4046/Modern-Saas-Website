de138932030ee470b2d74debd2df419f4696b569
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-24 13:32:40 +0100

    some finale changes to the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FOOTER_CONTENT } from "../constants"
 const Footer = () => {
   return (
     <footer className="mt-20 text-neutral-400">
-        <div className="max-w-7xl px-4 border-t border-neutral-800">
+        <div className="max-w-8xl px-4 border-t border-neutral-800">
             <div className="grid grid-cols-1 md:grid-cols-4 mt-20">
                 {FOOTER_CONTENT.sections.map((section, index) => (
                     <div key={index}>
